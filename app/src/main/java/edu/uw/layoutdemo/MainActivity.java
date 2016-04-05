0285ec2839f387429099ef5d17786aca7dc83ba1
Tin Ho <tintho@uw.edu>
2016-04-05 00:48:53 -0700

    end of class

--- app/src/main/java/edu/uw/layoutdemo/MainActivity.java
+++ app/src/main/java/edu/uw/layoutdemo/MainActivity.java
@@ -2,6 +2,11 @@ package edu.uw.layoutdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LAYOUT_DEMO";
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView) findViewById(R.id.picture);
+        //image.setImageResource(R.drawable.android_maneki);
+
+        //LayoutInflater inflator = getLayoutInflater();
+        //inflator.inflate(R.layout.image_content, R.id.theLinearLayout)
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewStub stub = (ViewStub)findViewById(R.id.stub);
+                if (stub != null) {
+                    stub.inflate();
+                }
+            }
+        });
     }
 }
